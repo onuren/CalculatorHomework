ef39cd532043c28e2fcef5eacc60e334b0dd41a4
onur engun <onurengun@outlook.com>
2021-01-20 03:06:13 +0300

    Add project files.

---
+++ basitHesapMakinesi/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basitHesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı girin:");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Yapmak istediğiniz işlemi girin:");
+            string islem = Console.ReadLine();
+            Console.WriteLine("İkinci sayıyı girin:");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            if (islem == "+")
+                Console.WriteLine("SONUÇ:"+ (sayi1 + sayi2));
+            else if (islem == "-")
+                Console.WriteLine("SONUÇ:" + (sayi1-sayi2));
+            else if (islem =="*")
+                Console.WriteLine("SONUÇ:" + (sayi1 *sayi2));
+            else if (islem == "/")
+                Console.WriteLine("SONUÇ:"+ (sayi1/sayi2));
+            else
+                Console.WriteLine("Lütfen sadece işlem sembolü giriniz");
+
+        }
+    }
+}
